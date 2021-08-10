fdaa06e87cfb27a65061bff558a637d73242a087
consumernidhog44 <88702240+consumernidhog44@users.noreply.github.com>
2021-08-10 14:33:25 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,42 @@
+$(function() {
+    $('.change-btn').click(function() {
+        var $displaySlide = $('.active');
+
+        $displaySlide.removeClass('active');
+
+        if($(this).hasClass('next')) {
+            $displaySlide.next().addClass('active');
+        } else {
+            $displaySlide.prev().addClass('active');
+        }
+
+        var slideIndex = $('.slide').index($('.active'));
+
+        $('.change-btn').show();
+
+        if(slideIndex == 0) {
+            $('.prev').hide();
+        } else if (slideIndex == $('.slide').length - 1) {
+            $('.next').hide();
+        }
+    });
+
+    $('#back-btn').click(function() {
+        $('html,body').animate({
+            'scrollTop':0
+        },'slow');
+    });
+
+    $('.qanda').click(function() {
+        var $answer = $(this).find('.answer');
+        if($answer.hasClass('open')) {
+            $answer.removeClass('open');
+            $answer.slideUp();
+        } else {
+            $answer.addClass('open');
+            $answer.slideDown();
+        }
+    });
+
+    
+});
